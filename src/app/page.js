bad046b02a5cc44ae0974cbe21f0970b4900505b
bearhtml <doraemoncoding@gmail.com>
2023-07-22 17:38:30 +0700

    d0ne

--- src/app/page.js
+++ src/app/page.js
@@ -10,24 +10,38 @@ import { useState } from "react";
 export default function Home() {
   //tasks = array of {id: string, title: string, completed: boolean}
   const [tasks, setTasks] = useState([]);
+  const [all,setall] = useState(0)
+  const [done,setdone] = useState(0)
 
   const addTask = (newTaskTitle) => {
     const newTask = { id: nanoid(), title: newTaskTitle, completed: false };
     const newTasks = [...tasks, newTask];
     setTasks(newTasks);
+    setall(all+1)
   };
 
   const deleteTask = (taskId) => {
+    const taskToDelete = tasks.find((task) => task.id === taskId);
+  
+    if (taskToDelete.completed) {
+      setdone((prevDone) => prevDone - 1);
+    }
+  
     const newTasks = tasks.filter((task) => task.id !== taskId);
     setTasks(newTasks);
+    setall((prevAll) => prevAll - 1);
   };
 
   const toggleDoneTask = (taskId) => {
-    //structuredClone will copy an array or an object "deeply"
-    //So objects within an object will be copied too
     const newTasks = structuredClone(tasks);
-    //search for a task based on condition
     const task = newTasks.find((x) => x.id === taskId);
+  
+    if (task.completed) {
+      setdone((prevDone) => prevDone - 1);
+    } else {
+      setdone((prevDone) => prevDone + 1);
+    }
+  
     task.completed = !task.completed;
     setTasks(newTasks);
   };
@@ -41,7 +55,7 @@ export default function Home() {
       <div style={{ maxWidth: "400px" }} className="mx-auto">
         {/* Task summary */}
         <p className="text-center text-secondary fst-italic">
-          All (...) Done (...)
+          All ({all}) Done ({done})
         </p>
         {/* task input */}
         <TaskInput addTaskFunc={addTask} />
@@ -60,7 +74,7 @@ export default function Home() {
       </div>
 
       {/* //footer section */}
-      <Footer year="2023" fullName="Chayanin Suatap" studentId="12345678" />
+      <Footer year="2023" fullName="Pachara J" studentId="650610789" />
     </div>
   );
 }
